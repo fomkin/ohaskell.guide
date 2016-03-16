559d1ca5b012239f39e981541ac0e355bbb9feaf
Denis Shevchenko <me@dshevchenko.biz>
2016-03-16 18:15:28 +0400

    Продолжаем.

--- src/CreatePdf.hs
+++ src/CreatePdf.hs
@@ -8,7 +8,8 @@ import System.Process (callCommand)
 
 createPdf :: FilePath -> IO ()
 createPdf pathToSingleMarkdown =
-    callCommand $ concat [ "pandoc --latex-engine=xelatex --include-before-body="
+    callCommand $ concat [ "pandoc --latex-engine=xelatex"
+                         , " --include-before-body="
                          , headerLATEX
                          , " --toc"
                          , " -V mainfont=\"PT Serif\""
@@ -21,6 +22,8 @@ createPdf pathToSingleMarkdown =
                          , " -V geometry=\"headsep=0.5in\""
                          , " -V title=\"О Haskell по-человечески\""
                          , " -V author=\"Денис Шевченко\""
+                         , " -V colorlinks=\"true\""
+                         , " -V urlcolor=\"Blue\""
                          , " -V polyglossia-lang=\"ru\""
                          , " -V polyglossia-otherlangs=\"[en]\""
                          , " -o "
