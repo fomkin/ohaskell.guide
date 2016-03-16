fe6ab033c279c87bbd436035df44609cefbbd78e
Denis Shevchenko <me@dshevchenko.biz>
2016-03-16 22:12:32 +0400

    Почти нормальный PDF.

--- src/CreatePdf.hs
+++ src/CreatePdf.hs
@@ -9,29 +9,14 @@ import System.Process (callCommand)
 createPdf :: FilePath -> IO ()
 createPdf pathToSingleMarkdown =
     callCommand $ concat [ "pandoc --latex-engine=xelatex"
-                         , " --include-before-body="
-                         , headerLATEX
-                         , " --toc"
-                         , " -V mainfont=\"PT Serif\""
-                         , " -V monofont=\"Ubuntu Mono\""
-                         , " -V fontsize=11pt"
-                         , " -V toc-depth=1"
-                         , " -V documentclass=\"book\""
-                         , " -V classoption=\"oneside\""
-                         , " -V geometry=\"margin=1.2in\""
-                         , " -V geometry=\"headsep=0.5in\""
-                         , " -V title=\"О Haskell по-человечески\""
-                         , " -V author=\"Денис Шевченко\""
-                         , " -V colorlinks=\"true\""
-                         , " -V urlcolor=\"Blue\""
-                         , " -V polyglossia-lang=\"ru\""
-                         , " -V polyglossia-otherlangs=\"[en]\""
+                         , " --template="
+                         , template
                          , " -o "
                          , out
                          , " "
                          , pathToSingleMarkdown
                          ]
   where
-    headerLATEX = "pdf/header.tex"
-    out         = "pdf/ohaskell.pdf"
+    template = "pdf/template.tex"
+    out      = "pdf/ohaskell.pdf"
 
