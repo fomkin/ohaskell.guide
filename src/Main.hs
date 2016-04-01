086ff4696d29983adb92d631a526ecc8f5310487
Denis Shevchenko <me@dshevchenko.biz>
2016-04-01 17:56:20 +0400

    Выбор варианта книги при сборке.

--- src/Main.hs
+++ src/Main.hs
@@ -7,22 +7,77 @@ import CreateHtml
 import CreateHtmlTemplates
 
 import Control.Concurrent.Async
+import System.Environment
+import System.Exit
 
 main :: IO ()
 main = do
-    putStrLn "Build a new version of the book, be patient..."
-
     (pathToSingleMarkdown, chapterPoints) <- createSingleMarkdown
 
-    createHtmlTemplates chapterPoints
+    args <- getArgs
+    check args
 
-    pdfDesktopDone <- async $ createPdfDesktop pathToSingleMarkdown
-    pdfMobileDone  <- async $ createPdfMobile pathToSingleMarkdown
-    epubDone       <- async $ createEpub pathToSingleMarkdown
-    htmlDone       <- async $ createHtml chapterPoints
+    buildEpubIfNecessary args pathToSingleMarkdown
+    buildPdfIfNecessary  args pathToSingleMarkdown
+    buildHtmlIfNecessary args chapterPoints
 
-    wait pdfDesktopDone
-    wait pdfMobileDone
-    wait epubDone
-    wait htmlDone
+buildEpubIfNecessary :: [String] -> FilePath -> IO ()
+buildEpubIfNecessary args pathToSingleMarkdown =
+    if buildAllOrJust epub args then do
+        buildingVersion epub
+        createEpub pathToSingleMarkdown
+    else
+        return ()
+
+buildPdfIfNecessary :: [String] -> FilePath -> IO ()
+buildPdfIfNecessary args pathToSingleMarkdown =
+    if buildAllOrJust pdf args then do
+        -- Самые тяжеловесные операции.
+        buildingVersion pdf
+        pdfDesktopDone <- async $ createPdfDesktop pathToSingleMarkdown
+        pdfMobileDone  <- async $ createPdfMobile pathToSingleMarkdown
+        wait pdfDesktopDone
+        wait pdfMobileDone
+    else
+        return ()
+
+buildHtmlIfNecessary :: [String] -> [ChapterPoint] -> IO ()
+buildHtmlIfNecessary args chapterPoints =
+    if buildAllOrJust html args then do
+        buildingVersion html
+        createHtmlTemplates chapterPoints
+        -- Аргумент rebuild нужен для Hakyll.
+        withArgs ["rebuild"] $ createHtml chapterPoints
+    else
+        return ()
+
+check :: [String] -> IO ()
+check args =
+    if someInvalidArgs then do
+        putStrLn $ "Usage: ohaskell ["  ++ pdf ++
+                                    "|" ++ epub ++
+                                    "|" ++ html ++ "]"
+        exitFailure
+    else
+        return ()
+  where
+    someInvalidArgs = not . null $ filter invalid args
+    invalid arg     =    arg /= pdf
+                      && arg /= epub
+                      && arg /= html
+
+buildAllOrJust :: String -> [String] -> Bool
+buildAllOrJust some args = some `elem` args || null args
+
+pdf :: String
+pdf = "--pdf"
+
+epub :: String
+epub = "--epub"
+
+html :: String
+html = "--html"
+
+buildingVersion :: String -> IO ()
+buildingVersion ver = putStrLn $ " Build " ++ drop 2 ver ++ " version..."
 
